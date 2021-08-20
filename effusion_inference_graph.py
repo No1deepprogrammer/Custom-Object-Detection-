905f1a86e39e094f88c9807c8345932b1b0ac1fd
No1deepprogrammer <89183984+No1deepprogrammer@users.noreply.github.com>
2021-08-20 09:50:48 +0530

    Add files via upload

---
+++ effusion_inference_graph.py
@@ -0,0 +1,5 @@
+python effusion_inference_graph.py \
+    --input_type image_tensor \
+    --pipeline_config_path /home/chaudharyboy/chandanProject/models/research/object_detection/training/ssd_mobilenet_v1_pets.config \
+    --trained_checkpoint_prefix /home/chaudharyboy/chandanProject/models/research/object_detection/training/model.ckpt-91534 \
+    --output_directory effusion_graph
